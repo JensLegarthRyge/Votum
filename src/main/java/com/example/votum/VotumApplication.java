a955ebe113000d7a5f887b7580ba0194613cc969
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2022-03-30 12:55:32 +0200

    Refractoring + import of models to repo's

--- src/main/java/com/example/votum/VotumApplication.java
+++ src/main/java/com/example/votum/VotumApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotumApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VotumApplication.class, args);
     }
