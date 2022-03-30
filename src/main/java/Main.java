d38204611a50de1f1fa1f30e8ffa1e56b3380c46
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2022-03-30 12:52:35 +0200

    Refractoring + import of models to repo's

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,38 @@
-import repositories.DatabaseRepository;
+import Model.Repositories.UserRepository;
+import Model.Repositories.WishRepository;
+import Model.Repositories.WishlistRepository;
+import Model.User;
+import Model.Wish;
+import Model.Wishlist;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseRepository dr = new DatabaseRepository();
+        WishlistRepository wlr = new WishlistRepository();
+        WishRepository wr = new WishRepository();
+        UserRepository ur = new UserRepository();
 
-        dr.connectTo();
-        dr.disconnectFrom();
+
+        ArrayList<User> allUsers = ur.getAllUsers();
+        for (User cr:allUsers) {
+            System.out.println(cr.toString());
+        }
+        System.out.println("\n\n");
+
+
+        ArrayList<Wishlist> allWishlists = wlr.getAllWishlistsFromUserID(3);
+        for (Wishlist cwl:allWishlists) {
+            System.out.println(cwl.getTitle());
+        }
+        System.out.println("\n\n");
+
+
+        ArrayList<Wish> allWishes = wr.getAllWishesFromWishlistID(3);
+        for (Wish cr:allWishes) {
+            System.out.println(cr.toString());
+        }
+        System.out.println("\n\n");
+
     }
 }
